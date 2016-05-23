779cee5b4d9bde4417b5012deca090860ac0edbd
Ritesh Kumar <rkritesh078@gmail.com>
2016-05-23 11:38:50 +0530

    custom fonts

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,3 +12,18 @@ const socialHtml = app.templates.social;
 getDOMNode('social').innerHTML = socialHtml(social);
 
 getDOMNode('blog-details').innerHTML = projectsHtml(blogs);
+
+function lazyload (elem) {
+  const lazyLoadImage = elem.getAttribute('data-background');
+  const img = new Image();
+  img.src = lazyLoadImage;
+  img.onload = function () {
+    elem.style.backgroundImage = `url(${lazyLoadImage})`;
+  }
+}
+
+const lazyLoadElems = document.querySelectorAll('[data-background]');
+
+for(let i = 0 ; i<lazyLoadElems.length; i++){
+  lazyload(lazyLoadElems[i])
+}
