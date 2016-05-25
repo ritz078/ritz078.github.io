58b5d1f723f86f5ec43f35bc991e45bfacbd4838
Ritesh Kumar <rkritesh078@gmail.com>
2016-05-25 13:31:14 +0530

    made sprite

--- app/scripts/data.js
+++ app/scripts/data.js
@@ -3,7 +3,7 @@ const projects = {
     name: 'embed.js',
     desc: 'A JavaScript plugin that analyses the string and automatically embeds emojis, media, maps, tweets, code and services.',
     url: 'http://riteshkr.com/embed.js/',
-    image: 'ejs.png',
+    image: 'ejs',
     tags: [{
       name: 'Producthunt',
       url: 'https://www.producthunt.com/tech/embed-js',
@@ -14,32 +14,32 @@ const projects = {
     name: 'react-filters (WIP)',
     desc: ' A collection of customizable filters like radio, checkbox, select etc. written in React.',
     url: 'https://github.com/ritz078/react-filters',
-    image:'rf.png'
+    image:'rf'
   },{
     name: 'react-fuzzy-search',
     url: 'https://github.com/ritz078/react-fuzzy-search',
     desc:'A fuzzy search component written in React.',
-    image:'react.png'
+    image:'react'
   },{
     name: 'ng-embed',
     desc: 'An AngularJS filter/directive for embedding emojis, media, maps, tweets, code and services. Currently supports Angular 1.x',
     url: 'https://github.com/ritz078/ngEmbed',
-    image: 'ng.png',
+    image: 'ng',
   }, {
     name: 'photomosaic.js',
     desc: 'A vanilla JavaScript plugin to create a mosaic of an image using HTML5 Canvas.',
     url: 'https://github.com/ritz078/photomosaic.js',
-    image: 'js.jpg',
+    image: 'js',
   }, {
     name: 'javascript-plugin-boilerplate',
     desc: 'A boilerplate to write plugins in pure JavaScript (Rollup, Grunt, Babel, Mocha, Istanbul, semantic-release, eslint, ghooks)',
     url: 'https://github.com/ritz078/javascript-plugin-boilerplate',
-    image: 'js.jpg',
+    image: 'js',
   }, {
     name: 'slide.js',
     desc: 'A lightweight jQuery plugin to slide HTML elements of variable size and with Lazyload support.',
     url: 'https://github.com/ritz078/slide.js',
-    image: 'js.jpg',
+    image: 'js',
   }]
 };
 
@@ -69,36 +69,36 @@ const blogs = {
     name: 'Setting up an ES6 Project Using Babel and Browserify',
     url:'https://www.sitepoint.com/setting-up-es6-project-using-babel-browserify/',
     desc: 'How to use the power of ECMAScript 6 in your projects by using Babel and Browserify.',
-    image: 'js.jpg'
+    image: 'js'
   }, {
     name: 'Creating a Web App with MATLAB and the MEAN Stack',
     url: 'https://www.sitepoint.com/creating-a-web-app-with-matlab-and-the-mean-stack/',
     desc: 'How to develop a small demo to demonstrate how to have MATLAB and the MEAN stack working together.',
-    image: 'iot.png'
+    image: 'iot'
   }, {
     name: 'Understanding Block Formatting Contexts in CSS',
     url:'https://www.sitepoint.com/understanding-block-formatting-contexts-in-css/',
-    image:'css.png',
+    image:'css',
     desc: 'delves into the CSS spec to explain, with demos and diagrams, what is meant by block formatting contexts and how they affect page layout.'
   }, {
     name: 'A Beginner’s Guide to Handlebars',
     url: 'https://www.sitepoint.com/a-beginners-guide-to-handlebars/',
     desc:'The basic concepts of Handlebars and also examines its frequently used functions and syntaxes.',
-    image:'hbs.jpg'
+    image:'hbs'
   },{
     name: 'Writing Better JavaScript with Flow',
     desc:'Introduces you to Flow, its main features, and how to integrate it in your project to discover bugs in your JavaScript code.',
     url: 'https://www.sitepoint.com/writing-better-javascript-with-flow/',
-    image:'flow.png'
+    image:'flow'
   }, {
     name: 'Controlling a Motorbot Using Arduino and Node.js',
     desc: 'How to create a basic NodeBot and explains how to use Node.js to create a link between web applications and robots.',
     url : 'https://www.sitepoint.com/controlling-a-motorbot-using-arduino-and-node-js/',
-    image: 'arduino.png'
+    image: 'arduino'
   }, {
     name: 'Getting Started with Service Workers',
     desc: 'Shows how the network intercepting feature of Service Workers works, making a website that runs even when the user is offline.',
     url: 'https://www.sitepoint.com/getting-started-with-service-workers/',
-    image:'js.jpg'
+    image:'js'
   }]
 };
